23bb7dd52be758b31cab7c798677ea8db2a411b5
sharmin-mim <sharmin133mim@gmail.com>
2020-06-18 21:35:59 +0600

    contains sat, vat and def value. is used for gain optimization

---
+++ qfm_ad/src/qfm_ad/SVD_Log.java
@@ -0,0 +1,49 @@
+package qfm_ad;
+/*
+ * This class contains  Quartet_Id and its corresponding values of satisfied, violated and deferred quartets
+ * in respect to a partition
+ * 
+ * */
+
+import java.util.Objects;
+
+public class SVD_Log {
+	private int QuartetID;
+	private int sat;//number of satisfied quartet
+	private int vat;//number of violated quartet
+	private int def;//number of deferred quartet
+	public SVD_Log(int quartetID, int sat, int vat, int def) {
+		
+		QuartetID = quartetID;
+		this.sat = sat;
+		this.vat = vat;
+		this.def = def;
+	}
+	public int getQuartetID() {
+		return QuartetID;
+	}
+	public int getSat() {
+		return sat;
+	}
+	public int getVat() {
+		return vat;
+	}
+	public int getDef() {
+		return def;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(QuartetID);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof SVD_Log))
+			return false;
+		SVD_Log other = (SVD_Log) obj;
+		return QuartetID == other.QuartetID;
+	}
+	
+	
+}
