874dc8f26a63c303bf860458214cb826155d47a4
Sharmin Akter Mim <60011513+sharmin-mim@users.noreply.github.com>
2023-12-02 09:06:38 +0600

    Update GainList.java

--- qfm_ad/src/qfm_ad/GainList.java
+++ qfm_ad/src/qfm_ad/GainList.java
@@ -2,16 +2,26 @@
  * Author: Sharmin Akter Mim
  * Code for QFM-FI
  * */
-package qfm_ad;
-
+package qfm_fi;
 
 
 public class GainList {
-	   public Taxa taxa;
-	   public int val;
 
-	   public GainList(Taxa taxa, int val) {
-	      this.taxa = taxa;
-	      this.val = val;
-	   }
+
+	
+	 //If I give Taxa instead of taxaToMove, space complexity becomes high. That's why I am rolling back to previous implementation
+	public Taxa taxa;
+	public int val;		
+
+	public GainList(Taxa taxa, int val) {
+		
+		this.taxa = taxa;
+		this.val = val;
+
 	}
+
+
+
+
+
+}
